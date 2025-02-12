49150b1f572d79c83828d6080e426934cd53fcb8
Rocka2012 <159764466+Rocka2012@users.noreply.github.com>
2025-02-12 15:10:18 +0100

    added working audio slider
    yes

---
+++ GameJam2025/Assets/Maikel/Scripts/SettingsMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Slider musicSlider;
+    public Slider voiceSlider;
+    public AudioMixer audioMixer;  // Add this
+
+    private void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        voiceSlider.value = PlayerPrefs.GetFloat("VoiceVolume", 1f);
+
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        voiceSlider.onValueChanged.AddListener(SetVoiceVolume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20);
+    }
+
+    public void SetVoiceVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("VoiceVolume", volume);
+        audioMixer.SetFloat("VoiceVolume", Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20);
+    }
+}
