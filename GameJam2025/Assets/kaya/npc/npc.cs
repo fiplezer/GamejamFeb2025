8aa996c74037c4bb3f627080c1ea8e93efe22cc1
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2025-02-12 09:32:26 +0100

    npc base

---
+++ GameJam2025/Assets/kaya/npc/npc.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc : MonoBehaviour
+{
+    bool talk = true;
+    unlock unlock;
+    public string textbeforecheck;
+    public string textcheck;
+    public string textcheckfalse;
+    public string textchecktrue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        unlock = GetComponentInChildren<unlock>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (talk)
+            {
+                Debug.Log(textbeforecheck);
+                Debug.Log(textcheck);
+
+                if (unlock.unlocked)
+                {
+                    Debug.Log(textchecktrue);
+                }
+                else
+                {
+                    Debug.Log(textcheckfalse);
+                }
+                talk = false;
+            }
+        }
+       
+        
+    }
+}
