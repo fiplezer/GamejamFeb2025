a59da9acb64769c696765402e68184b7a10206a3
Rocka2012 <159764466+Rocka2012@users.noreply.github.com>
2025-02-12 13:40:53 +0100

    main menu added
    added menu
    working on volume sliders

---
+++ GameJam2025/Assets/Maikel/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsMenu;
+
+    void Start()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
