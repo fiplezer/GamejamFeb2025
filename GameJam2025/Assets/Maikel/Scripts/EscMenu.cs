32abf3b5348e22094d64f5fce05886161802a829
Rocka2012 <159764466+Rocka2012@users.noreply.github.com>
2025-02-13 14:15:00 +0100

    escape menu final

--- GameJam2025/Assets/Maikel/Scripts/EscMenu.cs
+++ GameJam2025/Assets/Maikel/Scripts/EscMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class EscMenu : MonoBehaviour
@@ -9,6 +10,8 @@ public class EscMenu : MonoBehaviour
     public Slider musicSlider;
     public Slider voiceSlider;
     public AudioMixer audioMixer;
+    public GameObject ingameMenu;
+    public bool PauseMenu = false;
 
     private void Start()
     {
@@ -17,6 +20,21 @@ public class EscMenu : MonoBehaviour
 
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         voiceSlider.onValueChanged.AddListener(SetVoiceVolume);
+
+        ingameMenu.SetActive(false);
+    }
+
+    public void Update()
+    {   if (Input.GetKeyDown(KeyCode.Escape) && PauseMenu == false)
+        {
+            ingameMenu.SetActive(true);
+            PauseMenu = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && PauseMenu == true) 
+        {
+            ingameMenu.SetActive(false);
+            PauseMenu = false;
+        }
     }
 
     public void SetMusicVolume(float volume)
@@ -29,5 +47,17 @@ public class EscMenu : MonoBehaviour
     {
         PlayerPrefs.SetFloat("VoiceVolume", volume);
         audioMixer.SetFloat("VoiceVolume", Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20);
+    }
+
+    public void StartMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
